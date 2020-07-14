3995080f49d468790c4d21ca33ca990f85656fe6
Fatimaalbahar <albaharfatema18@gmail.com>
2020-07-14 13:20:08 +0300

    Initial Commit

---
+++ kwtview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kwtview
+//
+//  Created by Fatima Al Bahar on 7/14/20.
+//  Copyright © 2020 Fatima Al Bahar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
